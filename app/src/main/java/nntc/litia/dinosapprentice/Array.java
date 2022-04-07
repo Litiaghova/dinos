24b99dde874bd7b4aef5482fbb92a55629790d06
Арина Литягова <andreevaarina1311@gmail.com>
2022-04-07 22:37:12 +0300

    Подготовка файлов и внесение их в массивы для подстановки в уровень

---
+++ app/src/main/java/nntc/litia/dinosapprentice/Array.java
@@ -0,0 +1,39 @@
+package nntc.litia.dinosapprentice;
+
+public class Array {
+
+    /* Массивы для первого уровня (Начало) */
+    // final - ключевое слово, разрешает использовать перменную, но запрещает изменять её
+    // Массив хранящий в себе картинку цифры
+    final int[] images1 = {
+            R.drawable.onelevel_zero,
+            R.drawable.onelevel_one,
+            R.drawable.onelevel_two,
+            R.drawable.onelevel_three,
+            R.drawable.onelevel_four,
+            R.drawable.onelevel_five,
+            R.drawable.onelevel_six,
+            R.drawable.onelevel_seven,
+            R.drawable.onelevel_eight,
+            R.drawable.onelevel_nine,
+    };
+    // Массив хранящий в себе значение цифры
+    final int[] power1 = {0,1,2,3,4,5,6,7,8,9,};
+
+    // Массив хранящий в себе название цифры
+    final int[] texts1 = {
+            R.string.lvl1text0,
+            R.string.lvl1text1,
+            R.string.lvl1text2,
+            R.string.lvl1text3,
+            R.string.lvl1text4,
+            R.string.lvl1text5,
+            R.string.lvl1text6,
+            R.string.lvl1text7,
+            R.string.lvl1text8,
+            R.string.lvl1text9,
+    };
+
+    /* Массив для первого уровня (Конец) */
+
+}
